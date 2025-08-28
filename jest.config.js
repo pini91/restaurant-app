fe6ecda7f16779e399893415b5fb26052e7229fa
pini91 <brenda.loncaric@gmail.com>
2025-08-28 19:34:04 -0400

    removed some spaces in the jest file

--- jest.config.js
+++ jest.config.js
@@ -1,37 +1,18 @@
-// module.exports = {
-//   testEnvironment: 'node',
-//   setupFilesAfterEnv: ['<rootDir>/__test__/setup.js'],
-//   collectCoverageFrom: [
-//     '**/*.js',
-//     '!node_modules/**',
-//     '!coverage/**',
-//     '!jest.config.js',
-//     '!__test__ /**'
-//   ],
-//   testMatch: ['**/__test__/**/*.js','**/?(*.)+(spec|test).js'],
-//   verbose: true,
-//   forceExit: true,
-//   clearMocks: true,
-//   resetMocks: true,
-//   restoreMocks: true,
-//   testTimeout: 10000
-// }
-
 module.exports = {
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/__test__/setup.js'], // plural "__tests__"
+  setupFilesAfterEnv: ['<rootDir>/__test__/setup.js'],
   collectCoverageFrom: [
     '**/*.js',
     '!node_modules/**',
     '!coverage/**',
     '!jest.config.js',
-    '!__tests__/**'  // remove space here
+    '!__test__ /**'
   ],
-  testMatch: ['**/__test__/**/*.js'], // remove space here
+  testMatch: ['**/__test__/**/*.js', '**/?(*.)+(spec|test).js'],
   verbose: true,
   forceExit: true,
   clearMocks: true,
   resetMocks: true,
   restoreMocks: true,
   testTimeout: 10000
-};
+}
