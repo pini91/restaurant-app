020513b89e754db5b0332fdcd8c1bb5ce5723518
pini91 <brenda.loncaric@gmail.com>
2025-08-28 19:31:57 -0400

    still on the jest and setup file

--- jest.config.js
+++ jest.config.js
@@ -1,18 +1,37 @@
+// module.exports = {
+//   testEnvironment: 'node',
+//   setupFilesAfterEnv: ['<rootDir>/__test__/setup.js'],
+//   collectCoverageFrom: [
+//     '**/*.js',
+//     '!node_modules/**',
+//     '!coverage/**',
+//     '!jest.config.js',
+//     '!__test__ /**'
+//   ],
+//   testMatch: ['**/__test__/**/*.js','**/?(*.)+(spec|test).js'],
+//   verbose: true,
+//   forceExit: true,
+//   clearMocks: true,
+//   resetMocks: true,
+//   restoreMocks: true,
+//   testTimeout: 10000
+// }
+
 module.exports = {
   testEnvironment: 'node',
-  setupFilesAfterEnv: ['<rootDir>/__test__/setup.js'],
+  setupFilesAfterEnv: ['<rootDir>/__test__/setup.js'], // plural "__tests__"
   collectCoverageFrom: [
     '**/*.js',
     '!node_modules/**',
     '!coverage/**',
     '!jest.config.js',
-    '!__test__ /**'
+    '!__tests__/**'  // remove space here
   ],
-  testMatch: ['**/__test__/**/*.js','**/?(*.)+(spec|test).js'],
+  testMatch: ['**/__test__/**/*.js'], // remove space here
   verbose: true,
   forceExit: true,
   clearMocks: true,
   resetMocks: true,
   restoreMocks: true,
   testTimeout: 10000
-}
+};
