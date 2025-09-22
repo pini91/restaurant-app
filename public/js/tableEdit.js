aabc406d43d19ab72ee0cabd94ab1bda46736bed
pini91 <brenda.loncaric@gmail.com>
2025-09-22 12:46:50 -0400

    fixed the tableEdit.js

--- public/js/tableEdit.js
+++ public/js/tableEdit.js
@@ -23,7 +23,6 @@ async function tableNumber () {
 
     if (data === 'tooSmall') {
       alert('Please pick a table with seats for your group size')
-      location.reload()
     } else if (data === 'failed') {
       alert('Table is busy, please pick another hour or table')
     } else {
